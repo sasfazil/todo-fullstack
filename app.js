0a61515ee466cb40c23b571dbae52d182fb0b4cf
sasfazil <s.a.s.fazil@gmail.com>
2024-09-01 13:11:59 +0530

    modified

--- app.js
+++ app.js
@@ -5,14 +5,18 @@ const path = require("path");
 const bcrypt = require("bcryptjs");
 const { format } = require("date-fns");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 
 const dbPath = path.join(__dirname, "todoApp.db");
 const app = express();
-app.use(express.json());
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use(express.json());
 
 let db = null;
 
