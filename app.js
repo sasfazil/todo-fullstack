96a2ea7815afe75bb8ee222aa6d487474f878d52
sasfazil <s.a.s.fazil@gmail.com>
2024-09-02 18:38:28 +0530

    modified

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const dbPath = path.join(__dirname, "todoApp.db");
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 const corsOptions = {
-  origin: "https://todo-frontend-7jsn.onrender.com",
+  origin: "*",
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
